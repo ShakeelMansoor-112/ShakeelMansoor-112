1bba01e4a772daf2979f3de77fd3b2e93843d2d7
ShakeelMansoor-112 <159726644+ShakeelMansoor-112@users.noreply.github.com>
2024-03-20 16:03:34 +0500

    Simple Calculator

---
+++ index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Enter first number",
+        type: "number",
+        name: "firstnumber",
+    },
+    {
+        message: "Enter second number",
+        type: "number",
+        name: "secondnumber",
+    },
+    {
+        message: "Select one of the operator to perform operation",
+        type: "list",
+        name: "Operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.Operator == "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.Operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.Operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.Operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else
+    (console.log("Please choose valid option "));
